92250b3099072ca18f80de5cea52852560e63d6e
duytien812 <duytien0812@gmail.com>
2020-04-05 17:10:05 +0700

    1. Tạo project - import thư viện react-router-dom

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Chào mừng đến với React</h1>
+			</div>
+		);
+	}
+}
+
+//Cách viết Function component
+// function App() {
+//   return (
+//     <div>
+//       <Header />
+//     </div>
+//   );
+// }
+
+export default App;
