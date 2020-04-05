53dd2289f4fa41139e4cf43fc0a2aece7c257813
duytien812 <duytien0812@gmail.com>
2020-04-05 17:35:01 +0700

    70. React Router v4 : Tích Hợp Thư Viện 	- Tích hợp thư viện react-router-dom 	- Xây dựng Router cơ bản

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
 
 class App extends Component {
 	render() {
 		return (
-			<div>
-				<h1>Chào mừng đến với React</h1>
-			</div>
+			<Router>
+				<div className="App">
+					{/* Menu */}
+					<nav className="navbar navbar-inverse">
+						<ul className="nav navbar-nav">
+							<li className="active">
+								<a>Trang chủ</a>
+							</li>
+							<li>
+								<a>Liên hệ</a>
+							</li>
+						</ul>
+					</nav>
+					{/* Nội dung */}
+					<Route path="/" exact component={Home} />
+					<Route path="/about" component={About} />
+				</div>
+			</Router>
 		);
 	}
 }
-
-//Cách viết Function component
-// function App() {
-//   return (
-//     <div>
-//       <Header />
-//     </div>
-//   );
-// }
 
 export default App;
