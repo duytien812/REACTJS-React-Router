2406f955ad1139409965400194f409a770afda11
duytien812 <duytien0812@gmail.com>
2020-04-06 14:54:34 +0700

    78. React Router v4 : Đối Tượng Prompt 	- Xuất hiện Alert hỏi có muốn chuyển trang hay không?

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,46 @@
 import React, { Component } from 'react';
+import { Prompt } from 'react-router-dom';
 
 class Contact extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			isChecked: false
+		}
+	}
+
+	onClick = (isChecked) => {
+		this.setState({
+			isChecked: isChecked
+		})
+	}
+
 	render() {
+		var {isChecked} = this.state;
 		return (
 			<div>
 				<h1>Đây là trang liên hệ</h1>
+				
+				<button 
+					type="button" 
+					className="btn btn-info"
+					onClick={() => this.onClick(false)} 
+				>
+					Cho phép
+				</button> <br />
+				<button 
+					type="button" 
+					className="btn btn-danger" 
+					onClick={() => this.onClick(true)} 
+				>
+					Không cho phép
+				</button> <br />
+
+				<Prompt
+					when={isChecked}
+					message={location => (`Bạn chắc chắn muốn đi đến ${location.pathname}`)}
+				/>
 			</div>
 		);
 	}
